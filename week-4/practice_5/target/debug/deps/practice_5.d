d469bb4530f27069340170d8703c556aa2c5da00
Your Name <you@example.com>
2025-10-20 15:01:47 +0100

    (my eight commit)

---
+++ week-4/practice_5/target/debug/deps/practice_5.d
@@ -0,0 +1,5 @@
+C:\Users\tayem\t.marcus-olajireCOS101\week-4\practice_5\target\debug\deps\practice_5.d: src\main.rs
+
+C:\Users\tayem\t.marcus-olajireCOS101\week-4\practice_5\target\debug\deps\practice_5.exe: src\main.rs
+
+src\main.rs:
