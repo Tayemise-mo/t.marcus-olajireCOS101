2498379223dcab8bfb70e1e9d364d3cba1fe418f
Your Name <you@example.com>
2025-11-17 15:39:03 +0100

    (My eighteenth Commit)

---
+++ week-8/practice_10/target/debug/practice_10.d
@@ -0,0 +1,1 @@
+C:\Users\tayem\t.marcus-olajireCOS101\week-8\practice_10\target\debug\practice_10.exe: C:\Users\tayem\t.marcus-olajireCOS101\week-8\practice_10\src\main.rs
---
+++ week-8/practice_11/target/debug/practice_11.d
@@ -0,0 +1,1 @@
+C:\Users\tayem\t.marcus-olajireCOS101\week-8\practice_11\target\debug\practice_11.exe: C:\Users\tayem\t.marcus-olajireCOS101\week-8\practice_11\src\main.rs
